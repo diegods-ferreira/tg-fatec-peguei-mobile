f41f4f92b0b6aa29f0b9135ae9a6260cd8c67274
Diego Ferreira <diegosf1705@gmail.com>
2020-10-24 11:45:15 -0300

    fix: BottomTabs are now responsive

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Feather from 'react-native-vector-icons/Feather';
 
+import {
+  parseWidthPercentage,
+  parseHeightPercentage,
+} from '@utils/screenPercentage';
+
 import OrdersRoutes from './orders.routes';
 import TripsRoutes from './trips.routes';
 import ChatsRoutes from './chats.routes';
@@ -28,14 +33,28 @@ const AppRoutes: React.FC = () => {
             iconName = 'user';
           }
 
-          return <Feather name={iconName} size={24} color={iconColor} />;
+          return (
+            <Feather
+              name={iconName}
+              size={parseHeightPercentage(24)}
+              color={iconColor}
+            />
+          );
         },
       })}
       tabBarOptions={{
         activeBackgroundColor: '#ff8c42',
         inactiveBackgroundColor: '#312e38',
         showLabel: false,
-        style: { height: 56 },
+        style: {
+          height: parseHeightPercentage(56),
+          borderTopColor: '#312e38',
+          elevation: 8,
+          shadowOffset: { width: 5, height: 5 },
+          shadowColor: '#000000',
+          shadowOpacity: 0.25,
+          shadowRadius: 8,
+        },
       }}
     >
       <BottomTab.Screen name="OrdersRoutes" component={OrdersRoutes} />
