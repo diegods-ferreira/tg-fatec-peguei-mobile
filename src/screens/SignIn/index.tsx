052bc05676db4c928fee5e042883faaafdaf0b9c
Diego Ferreira <diegosf1705@gmail.com>
2020-10-28 19:31:51 -0300

    chore: Removing react-native-modals package

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
+  Alert,
   Image,
   ImageBackground,
   KeyboardAvoidingView,
@@ -17,10 +18,6 @@ import { useAuth } from '@hooks/auth';
 
 import Input from '@components/Input';
 import Button from '@components/Button';
-import CustomModal from '@components/Modals/CustomModal';
-import MessageModal, {
-  MessageModalProps,
-} from '@components/Modals/MessageModal';
 
 import logoImg from '@assets/Logo.png';
 import backgroundImg from '@assets/bg-image.png';
@@ -39,13 +36,6 @@ import {
   ResetPasswordButtonText,
   SignUpButton,
   SignUpButtonText,
-  ModalTitle,
-  ModalSubtitle,
-  ModalButtonsContainer,
-  ModalCancelButton,
-  ModalCancelButtonText,
-  ModalSendButton,
-  ModalButtonText,
 } from './styles';
 
 interface SignInFormData {
@@ -56,30 +46,10 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
-  const resetPasswordFormRef = useRef<FormHandles>(null);
 
   const navigation = useNavigation();
 
   const { signIn } = useAuth();
-
-  const [
-    isResetPasswordModalVisible,
-    setIsResetPasswordModalVisible,
-  ] = useState(false);
-  const [messageModalProps, setMessageModalProps] = useState<
-    Omit<MessageModalProps, 'setIsVisible'>
-  >({} as MessageModalProps);
-
-  const toggleResetPasswordModal = useCallback(() => {
-    setIsResetPasswordModalVisible(!isResetPasswordModalVisible);
-  }, [isResetPasswordModalVisible]);
-
-  const toggleMessageModal = useCallback(() => {
-    setMessageModalProps({
-      ...messageModalProps,
-      isVisible: !messageModalProps.isVisible,
-    });
-  }, [messageModalProps]);
 
   const handleNavigateToSignUp = useCallback(() => {
     navigation.navigate('SignUp');
@@ -112,129 +82,82 @@ const SignIn: React.FC = () => {
           return;
         }
 
-        setMessageModalProps({
-          title: 'Erro na autenticação',
-          message: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
-          type: 'error',
-          isVisible: true,
-        });
+        Alert.alert(
+          'Erro',
+          'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        );
       }
     },
     [signIn],
   );
 
   return (
-    <>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        enabled
-      >
-        <ImageBackground source={backgroundImg} style={{ flex: 1 }}>
-          <Container>
-            <LogoImage source={logoImg} />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ImageBackground source={backgroundImg} style={{ flex: 1 }}>
+        <Container>
+          <LogoImage source={logoImg} />
 
-            <LoginContainer>
-              <View>
-                <Title>Faça login</Title>
-              </View>
+          <LoginContainer>
+            <View>
+              <Title>Faça login</Title>
+            </View>
 
-              <SocialLoginContainer>
-                <SocialLoginText>Usar</SocialLoginText>
-                <SocialLoginOption>
-                  <Image source={googleLogo} />
-                </SocialLoginOption>
-                <SocialLoginOption>
-                  <Image source={facebookLogo} />
-                </SocialLoginOption>
-              </SocialLoginContainer>
-            </LoginContainer>
+            <SocialLoginContainer>
+              <SocialLoginText>Usar</SocialLoginText>
+              <SocialLoginOption>
+                <Image source={googleLogo} />
+              </SocialLoginOption>
+              <SocialLoginOption>
+                <Image source={facebookLogo} />
+              </SocialLoginOption>
+            </SocialLoginContainer>
+          </LoginContainer>
 
-            <Form ref={formRef} onSubmit={handleSignIn}>
-              <Input
-                name="email"
-                icon="mail"
-                placeholder="E-mail"
-                autoCorrect={false}
-                keyboardType="email-address"
-                autoCapitalize="none"
-                returnKeyType="next"
-                onSubmitEditing={() => passwordInputRef.current?.focus()}
-              />
+          <Form ref={formRef} onSubmit={handleSignIn}>
+            <Input
+              name="email"
+              icon="mail"
+              placeholder="E-mail"
+              autoCorrect={false}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              returnKeyType="next"
+              onSubmitEditing={() => passwordInputRef.current?.focus()}
+            />
 
-              <Input
-                ref={passwordInputRef}
-                name="password"
-                icon="lock"
-                placeholder="Senha"
-                toggleContentVisibilityButton
-                secureTextEntry
-                autoCapitalize="none"
-                returnKeyType="send"
-                onSubmitEditing={() => formRef.current?.submitForm()}
-              />
+            <Input
+              ref={passwordInputRef}
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+              toggleContentVisibilityButton
+              secureTextEntry
+              autoCapitalize="none"
+              returnKeyType="send"
+              onSubmitEditing={() => formRef.current?.submitForm()}
+            />
 
-              <Button onPress={() => formRef.current?.submitForm()}>
-                Entrar
-              </Button>
-            </Form>
+            <Button onPress={() => formRef.current?.submitForm()}>
+              Entrar
+            </Button>
+          </Form>
 
-            <ResetPasswordButton onPress={toggleResetPasswordModal}>
-              <ResetPasswordButtonText>
-                Esqueci minha senha
-              </ResetPasswordButtonText>
-            </ResetPasswordButton>
+          <ResetPasswordButton onPress={() => {}}>
+            <ResetPasswordButtonText>
+              Esqueci minha senha
+            </ResetPasswordButtonText>
+          </ResetPasswordButton>
 
-            <SignUpButton onPress={handleNavigateToSignUp}>
-              <SignUpButtonText>Criar uma conta</SignUpButtonText>
-            </SignUpButton>
-          </Container>
-        </ImageBackground>
-      </KeyboardAvoidingView>
-
-      <CustomModal
-        isVisible={isResetPasswordModalVisible}
-        setIsVisible={toggleResetPasswordModal}
-      >
-        <ModalTitle>Recuperar senha</ModalTitle>
-
-        <ModalSubtitle>
-          Enviaremos um e-mail de recuperação de senha para você.
-        </ModalSubtitle>
-
-        <Form ref={resetPasswordFormRef} onSubmit={() => {}}>
-          <Input
-            name="email"
-            icon="mail"
-            placeholder="E-mail"
-            autoCorrect={false}
-            keyboardType="email-address"
-            autoCapitalize="none"
-            returnKeyType="next"
-            onSubmitEditing={() => passwordInputRef.current?.focus()}
-          />
-
-          <ModalButtonsContainer>
-            <ModalCancelButton onPress={toggleResetPasswordModal}>
-              <ModalCancelButtonText>Cancelar</ModalCancelButtonText>
-            </ModalCancelButton>
-
-            <ModalSendButton onPress={() => formRef.current?.submitForm()}>
-              <ModalButtonText>Enviar</ModalButtonText>
-            </ModalSendButton>
-          </ModalButtonsContainer>
-        </Form>
-      </CustomModal>
-
-      <MessageModal
-        title={messageModalProps.title}
-        subtitle={messageModalProps.subtitle}
-        message={messageModalProps.message}
-        type={messageModalProps.type}
-        isVisible={messageModalProps.isVisible || false}
-        setIsVisible={toggleMessageModal}
-      />
-    </>
+          <SignUpButton onPress={handleNavigateToSignUp}>
+            <SignUpButtonText>Criar uma conta</SignUpButtonText>
+          </SignUpButton>
+        </Container>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
